0b265c6ae56c259bd1ef2513e41f241b51cd3cad
xiao-dou-bi <3466976215@qq.com>
2020-02-23 18:10:08 +0800

    完整的项目

---
+++ supermall/vue.config.js
@@ -0,0 +1,15 @@
+// modules.export={
+//   configureWebpack:{
+//     resolove:{
+//       // extensions 文件后缀名省略 默认已经配置好了.js .vue等 不做配置
+//       alias:{
+//         // 脚手架3表示路径可以用@代替src/
+//         'assets': '@/assets',
+//         'views': '@/views',
+//         'common': '@/common',
+//         'components': '@/components',
+//         'network': '@/network',
+//       }
+//     },
+//   }
+// }
